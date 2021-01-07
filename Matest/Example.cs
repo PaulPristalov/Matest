46b3d3723ba40ac0b66c2ab271647d52c121acaf
Павел Присталов <pristalov.forken@mail.ru>
2021-01-07 18:15:11 +0400

    The interface was created and the example generator was started creating

---
+++ Matest/Example.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matest
+{
+    /// <summary>
+    /// Contain example
+    /// </summary>
+    class Example
+    {
+        /// <summary>
+        /// Full example in a string
+        /// </summary>
+        public string ExampleStr { get; }
+        /// <summary>
+        /// First Operand
+        /// </summary>
+        public int Operand1 { get; }
+        /// <summary>
+        /// Second operand
+        /// </summary>
+        public int Operand2 { get; }
+        /// <summary>
+        /// Sign of an example
+        /// </summary>
+        public char Sign { get; }
+
+        public Example(int op1, int op2, char sign)
+        {
+            Operand1 = op1;
+            Operand2 = op2;
+            Sign = sign;
+
+            ExampleStr = $"{Operand1} {Sign} {Operand2}";
+        }
+
+
+    }
+}
---
+++ Matest/ExampleGenerator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Matest
+{
+    /// <summary>
+    /// Generate examples
+    /// </summary>
+    class ExampleGenerator
+    {
+        /// <summary>
+        /// Generate new example
+        /// </summary>
+        /// <returns></returns>
+        public static Example NewExample()
+        {
+            // Sign of an example
+            char sign = GetSign();
+
+            /* TODO: создать файл с настройками
+             * реализовать получение мин и макс значений у знака
+             * реализовать генерацию операндов по настройкам
+             */
+
+            return null;
+
+        }
+
+        /// <summary>
+        /// Generate random sign of an example
+        /// </summary>
+        /// <returns></returns>
+        private static char GetSign()
+        {
+            char sign = '+';
+
+            // Random int value, wich convert to char
+            var rnd = new Random();
+            int s = rnd.Next(1, 5);
+
+            switch (s)
+            {
+                case 1:
+                    sign = '+';
+                    break;
+                case 2:
+                    sign = '-';
+                    break;
+                case 3:
+                    sign = '*';
+                    break;
+                case 4:
+                    sign = '/';
+                    break;
+            }
+
+            return sign;
+        }
+    }
+}
